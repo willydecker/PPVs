f58441cb4804851575bb303d0629be570ea7a19d
Brian Capouch <brianc@saintjoe.edu>
2015-02-24 16:46:54 -0500

    Drawing functions

---
+++ drawFuncs.js
@@ -0,0 +1,21 @@
+function drawTable(rows) {
+  var heights = rowHeights(rows);
+  var widths = colWidths(rows);
+
+  function drawLine(blocks, lineNo) {
+    return blocks.map(function(block) {
+      return block[lineNo];
+    }).join(" ");
+  }
+
+  function drawRow(row, rowNum) {
+    var blocks = row.map(function(cell, colNum) {
+      return cell.draw(widths[colNum], heights[rowNum]);
+    });
+    return blocks[0].map(function(_, lineNo) {
+      return drawLine(blocks, lineNo);
+    }).join("\n");
+  }
+  return rows.map(drawRow).join("\n");
+}
+
