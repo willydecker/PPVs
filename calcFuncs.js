a1c5ae0520b1334d51d7aaeed4d631ab93e01c63
Brian Capouch <brianc@saintjoe.edu>
2015-02-24 16:52:37 -0500

    Add minimal comments

--- calcFuncs.js
+++ calcFuncs.js
@@ -1,3 +1,4 @@
+// Uses cell data to generate a mapped array of necessary heights
 function rowHeights(rows) {
   return rows.map(function(row) {
     return row.reduce(function(max, cell) {
@@ -6,8 +7,9 @@ function rowHeights(rows) {
   });
 }
 
+// Uses cell data to generate a mapped array of necessary widths
 function colWidths(rows) {
-  return rows[0].map(function(_, i) {
+  return rows[0].map(function(_, i) { // Note use of "dead" parameter
     return rows.reduce(function(max, row) {
       return Math.max(max, row[i].minWidth());
     }, 0);
