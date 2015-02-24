df96dc90ec45bc74865f2abe3b162617bd006edb
Brian Capouch <brianc@saintjoe.edu>
2015-02-24 17:45:02 -0500

    Added more detailed comments

--- calcFuncs.js
+++ calcFuncs.js
@@ -1,4 +1,6 @@
 // Uses cell data to generate a mapped array of necessary heights
+// Input: an array rows, each element is an array of heights
+//	Returns: one-dimensional array, each is height of a single row
 function rowHeights(rows) {
   return rows.map(function(row) {
     return row.reduce(function(max, cell) {
@@ -8,6 +10,7 @@ function rowHeights(rows) {
 }
 
 // Uses cell data to generate a mapped array of necessary widths
+// Inputs and outputs are as above, except widths instead of heights
 function colWidths(rows) {
   return rows[0].map(function(_, i) { // Note use of "dead" parameter
     return rows.reduce(function(max, row) {
