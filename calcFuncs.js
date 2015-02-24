514777ef8e456c9d685333d90edfb1b162fb6d4a
Brian Capouch <brianc@saintjoe.edu>
2015-02-24 16:46:16 -0500

    Functions to calculate height and width

---
+++ calcFuncs.js
@@ -0,0 +1,16 @@
+function rowHeights(rows) {
+  return rows.map(function(row) {
+    return row.reduce(function(max, cell) {
+      return Math.max(max, cell.minHeight());
+    }, 0);
+  });
+}
+
+function colWidths(rows) {
+  return rows[0].map(function(_, i) {
+    return rows.reduce(function(max, row) {
+      return Math.max(max, row[i].minWidth());
+    }, 0);
+  });
+}
+
