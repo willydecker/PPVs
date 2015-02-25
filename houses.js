b063d8ce6efae0f7800c05cfeb196c67ab1d426a
Brian Capouch <brianc@saintjoe.edu>
2015-02-24 21:46:12 -0500

    Shorten field for better look

--- houses.js
+++ houses.js
@@ -3,7 +3,7 @@ var HOUSES = [
   {name: "Hackley", constructed: 1876, style: "H-Plan", builder: "Elias W. and Mary Malcolm Horner"},
   {name: "Posey", constructed: 1892, style: "Italianate", builder: "Cyrus L. and Martha Poisel Posey"},
   {name: "Bremer", constructed: 1908, style: "Bungalow", builder: "Theodore and Mary Bremer"},
-  {name: "Nicoles", constructed: 1889, style: "Arts and Crafts Foursquare", builder: "Frank and Mattie Farnsley Nicoles"},
+  {name: "Nicoles", constructed: 1889, style: "Craftsman Foursquare", builder: "Frank and Mattie Farnsley Nicoles"},
   {name: "Low", constructed: 1888, style: "Italianate", builder: "Simpson and Florence Faris Low"},
   {name: "Horner", constructed: 1908, style: "Pyramidal", builder: "Claude and Blanche Rowlands Horner"},
   {name: "Logan", constructed: 1908, style: "Queen Anne Cottage", builder: "Arthur C. and Lillian Reed Prevo"}
