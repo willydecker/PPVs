d7314463ae0889162a1d474590a3ad56c9ab4b2f
Brian Capouch <brianc@saintjoe.edu>
2015-02-24 21:44:14 -0500

    Add builder field

--- houses.js
+++ houses.js
@@ -1,12 +1,12 @@
 var HOUSES = [
-  {name: "Shultz", constructed: 1853, style: "Federal"},
-  {name: "Hackley", constructed: 1876, style: "H-Plan"},
-  {name: "Posey", constructed: 1892, style: "Italianate"},
-  {name: "Bremer", constructed: 1908, style: "Bungalow"},
-  {name: "Nicoles", constructed: 1889, style: "Arts and Crafts Foursquare"},
-  {name: "Low", constructed: 1888, style: "Italianate"},
-  {name: "Horner", constructed: 1908, style: "Pyramidal"},
-  {name: "Logan", constructed: 1908, style: "Queen Anne Cottage"}
+  {name: "Shultz", constructed: 1853, style: "Federal", builder: "Joseph B. and Elizabeth Meek Shultz"},
+  {name: "Hackley", constructed: 1876, style: "H-Plan", builder: "Elias W. and Mary Malcolm Horner"},
+  {name: "Posey", constructed: 1892, style: "Italianate", builder: "Cyrus L. and Martha Poisel Posey"},
+  {name: "Bremer", constructed: 1908, style: "Bungalow", builder: "Theodore and Mary Bremer"},
+  {name: "Nicoles", constructed: 1889, style: "Arts and Crafts Foursquare", builder: "Frank and Mattie Farnsley Nicoles"},
+  {name: "Low", constructed: 1888, style: "Italianate", builder: "Simpson and Florence Faris Low"},
+  {name: "Horner", constructed: 1908, style: "Pyramidal", builder: "Claude and Blanche Horner"},
+  {name: "Logan", constructed: 1908, style: "Queen Anne Cottage", builder: "Arthur C. and Lillian Prevo"}
 ];
 
 if (typeof module != "undefined" && module.exports)
