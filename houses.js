5653cc72bbbd0d2c4c803863876ca7c63cab0aa6
Brian Capouch <brianc@saintjoe.edu>
2015-02-24 21:49:50 -0500

    Use preferred style name

--- houses.js
+++ houses.js
@@ -2,7 +2,7 @@ var HOUSES = [
   {name: "Shultz", constructed: 1853, style: "Federal", builders: "Joseph B. and Elizabeth Meek Shultz"},
   {name: "Hackley", constructed: 1876, style: "H-Plan", builders: "Elias W. and Mary Malcolm Horner"},
   {name: "Posey", constructed: 1892, style: "Italianate", builders: "Cyrus L. and Martha Poisel Posey"},
-  {name: "Bremer", constructed: 1908, style: "Bungalow", builders: "Theodore and Mary Bremer"},
+  {name: "Bremer", constructed: 1908, style: "Craftsman", builders: "Theodore and Mary Bremer"},
   {name: "Nicoles", constructed: 1889, style: "Craftsman Foursquare", builders: "Frank and Mattie Farnsley Nicoles"},
   {name: "Low", constructed: 1888, style: "Italianate", builders: "Simpson and Florence Faris Low"},
   {name: "Horner", constructed: 1908, style: "Pyramidal", builders: "Claude and Blanche Rowlands Horner"},
