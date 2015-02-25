9d50460728f6511e7803760ba70b39b98f191bf9
Brian Capouch <brianc@saintjoe.edu>
2015-02-24 19:49:23 -0500

    Dataset for Capouch's old houses

---
+++ houses.js
@@ -0,0 +1,13 @@
+var HOUSES = [
+  {name: "Shultz", constructed: 1853, style: "Federal"},
+  {name: "Hackley", constructed: 1876, style: "H-Plan"},
+  {name: "Posey", constructed: 1892, style: "Italianate"},
+  {name: "Bremer", constructed: 1908, style: "Bungalow"},
+  {name: "Nicoles", constructed: 1889, style: "Arts and Crafts Foursquare"},
+  {name: "Low", constructed: 1888, style: "Italianate"},
+  {name: "Horner", constructed: 1908, style: "Pyramidal"},
+  {name: "Logan", constructed: 1908, style: "Queen Anne Cottage"}
+];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = HOUSES;
