a1e5b1e70912fcb0b258eceab7c9cfe20b042171
Brian Capouch <brianc@saintjoe.edu>
2015-03-03 17:25:24 -0500

    Set for printing larder dataset, add some sugar

--- printDataRJ.js
+++ printDataRJ.js
@@ -1,4 +1,3 @@
-// Bring in the pieces
 load('calcFuncs.js');
 load('drawFuncs.js');
 load('TextCell.js');
@@ -6,7 +5,17 @@ load('UnderlinedCell.js');
 load('RTextCell.js');
 
 // Here's where you load the dataset
-load('mountains.js');
+// load('mountains.js');
+// load('houses.js');
+load('larder.js');
+
+// As suggested by stackoverflow post
+// Original regex for safekeeping
+// var dateReg = /^\d{2}[.-/]\d{2}[.-/]\d{4}$/
+
+// Set whether or not to right-justify dates
+var rightJustifyDates = false;
+var dateReg = (rightJustifyDates?/^(\d{2}|\d)[.-/](\d|\d{2})[.-/]\d{2}$/:null);
 
 function dataTable(data) {
   var keys = Object.keys(data[0]);
@@ -17,13 +26,19 @@ function dataTable(data) {
     return keys.map(function(name) {
       var value = row[name];
       // This was changed:
-      if (typeof value == "number")
+      // Here is where new date type test will go
+//if(typeof value == "string")
+//print('Matching: ' + value + ' with ' + value.match(dateReg));
+      if ((typeof value == "number"))
         return new RTextCell(String(value));
       else
-        return new TextCell(String(value));
+        if(typeof value == "string" && value.match(dateReg))
+	  return new RTextCell(String(value));
+	else
+          return new TextCell(String(value));
     });
   });
   return [headers].concat(body);
 }
 
-print(drawTable(dataTable(MOUNTAINS)));
+print(drawTable(dataTable(LARDER_FILE)));
