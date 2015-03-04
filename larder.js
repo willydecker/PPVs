3afe3991b02f0880347ab0e8a66f33c1b796ac17
Brian Capouch <brianc@saintjoe.edu>
2015-03-04 04:18:23 -0500

    Dates are an important part of my dataset

--- larder.js
+++ larder.js
@@ -4,7 +4,7 @@ var LARDER_FILE = [
 { "labelID": "26", "name": "milk", "quantity": 1, "units": "half-gal", "dateIn": "2/6/15", "life": 8  },
 { "labelID": "27", "name": "butter", "quantity": 2, "units": "lb", "dateIn": "2/1/15", "life": 20  },
 { "labelID": "28", "name": "salt", "quantity": 1, "units": "lb", "dateIn": "1/20/15", "life": 365  },
-{ "labelID": "29", "name": "Cream cheese", "quantity": 3, "units": "lb", "dateIn": "1/10/15", "life": 45 },
+{ "labelID": "29", "name": "Cream cheese", "quantity": 3, "units": "lb", "dateIn": "1-10-15", "life": 45 },
 { "labelID": "30", "name": "Bread flour", "quantity": 10, "units": "lb", "dateIn": "2/7/15", "life": 45  },
 { "labelID": "31", "name": "Chicken Stock", "quantity": 1, "units": "gal", "dateIn": "1/22/15", "life": 10  },
 { "labelID": "32", "name": "Mexican rice", "quantity": 1, "units": "gl", "remark": "UNSALTED!!", "dateIn": "1/28/15", "life": 20  }
