09447849d48543ca0a2de8c9b85460eeecde0602
Brian Capouch <brianc@saintjoe.edu>
2015-03-07 00:55:18 -0500

    Comment added to test upstream merge feature

--- printDataRJ.js
+++ printDataRJ.js
@@ -10,8 +10,8 @@ load('RTextCell.js');
 load('larder.js');
 
 // This code does date recognition so they can be right-justified
-//   You can turn it on and off
-var rightJustifyDates = false;
+//   You can turn it on and off with this variable
+var rightJustifyDates = true;
 // This regular expression contains logic-by-pattern to identify a date
 var dateReg = (rightJustifyDates?/^(\d{2}|\d)[.-/](\d|\d{2})[.-/]\d{2}$/:null);
 
