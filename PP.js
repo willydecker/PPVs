d2fb172946c393f5352fa50db63860123a09f699
willydecker <wdw1062@saintjoe.edu>
2015-03-12 18:18:46 -0400

    this is the pay per view repo code

---
+++ PP.js
@@ -0,0 +1,15 @@
+//this is the pay per view of the houses repo
+var PPVS = [
+	{name: "Backlash", debut: April 25 1999, # of Matches: 9},
+	{name: "Summerslam", debut: August 29 1988, # of Matches: 10},
+	{name: "Wrestlemania", debut: March 31 1985, # of Matches: 9},
+	{name: "Hell in a Cell", debut: October 4 2009, # of Matches: 8},
+	{name: "Survivor Series", debut: November 26 1987, # of Matches: 8},
+	{name: "Royal Rumble", debut: January 15 1989, # of Matches: 7},
+	{name: "Armageddon", debut: December 12 1999, # of Matches: 10},
+	{name: "Extreme Rules", debut: June 7 2009, # of Matches: 11},
+	{name: "Money in the Bank", debut: July 18 2010, # of Matches: 9}
+	];
+
+if (typeof module != "undefined" && module.exports)
+	module.exports = PPVS;
